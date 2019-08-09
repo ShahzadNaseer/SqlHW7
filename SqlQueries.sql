d0dd81bf398d242035c696abe471a324c8b592cc
SNaseerTX <34410147+ShahzadNaseer@users.noreply.github.com>
2019-08-09 13:36:19 -0500

    Final version of Sql Queries.

--- SqlQueries.sql
+++ SqlQueries.sql
@@ -1,4 +1,5 @@
--- 1
+-- 1. List the following details of each employee: employee number, last name, first name, gender, and salary.
+
 SELECT 	e.emp_no "Employee Number", last_name "Last Name", 
 		first_name "First Name",
 		CASE WHEN e.gender = 'M' THEN 'Male' ELSE 
@@ -9,7 +10,7 @@ SELECT 	e.emp_no "Employee Number", last_name "Last Name",
   		salaries s
  WHERE e.emp_no = s.emp_no
  
--- 2
+-- 2. List employees who were hired in 1986.
 
 SELECT 	e.emp_no "Employee Number", e.birth_date "Birth Date",
 		last_name "Last Name", first_name "First Name",
@@ -20,7 +21,8 @@ SELECT 	e.emp_no "Employee Number", e.birth_date "Birth Date",
   FROM employees e
  WHERE date_part('year', e.hire_date) = 1986
  
--- 3
+-- 3. List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name, and start and end employment dates.
+
 select 	d.dept_no "Dept Number", d.dept_name "Department", dm.emp_no "Employee Number",
 		e.last_name "Last Name", e.first_name "First Name",
 		dm.from_date "Start Date", dm.to_date "End Date",
@@ -32,7 +34,8 @@ select 	d.dept_no "Dept Number", d.dept_name "Department", dm.emp_no "Employee N
    and dm.dept_no = d.dept_no
  order by 2, dm.to_date desc
  
---4 all ex-employees are in the last
+-- 4. List the department of each employee with the following information: employee number, last name, first name, and department name.
+
   select e.emp_no "Employee Number",
 		e.last_name "Last Name", e.first_name "First Name",
 		cd.dept_name "Department"
@@ -48,7 +51,8 @@ select 	d.dept_no "Dept Number", d.dept_name "Department", dm.emp_no "Employee N
  	on e.emp_no = cd.emp_no  
  order by 4, 1
  
--- 5
+-- 5. List all employees whose first name is "Hercules" and last names begin with "B."
+
 select 	e.emp_no "Employee Number", last_name "Last Name", 
 		first_name "First Name", birth_date "Birth Date",
 		CASE WHEN e.gender = 'M' THEN 'Male' ELSE 
@@ -60,7 +64,8 @@ select 	e.emp_no "Employee Number", last_name "Last Name",
    and first_name = 'Hercules'
  order by 2, 3
  
--- 6
+-- 6. List all employees in the Sales department, including their employee number, last name, first name, and department name.
+
 select e.emp_no "Employee Number", last_name "Last Name", 
 		first_name "First Name", dept_name "Department"
    from employees e,
@@ -71,7 +76,8 @@ select e.emp_no "Employee Number", last_name "Last Name",
    and d.dept_name = 'Sales'
  order by 2, 3
  
---7
+--7. List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+
 select e.emp_no "Employee Number", last_name "Last Name", 
 		first_name "First Name", dept_name "Department"
    from employees e,
@@ -82,15 +88,16 @@ select e.emp_no "Employee Number", last_name "Last Name",
    and d.dept_name in ( 'Development', 'Sales')
  order by 4, 2, 3
  
--- 8 Easy Way
+-- 8. In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
 select  last_name "Last Name", count(emp_no)
   from employees e
  group by last_name
- order by 1
--- 8
+ order by 2 desc
+ 
+-- 8 Alternative
 select e.emp_no "Employee Number", last_name "Last Name", 
 		first_name "First Name",
 		e.gender "Gender",
 		count(1) OVER (PARTITION BY  e.last_name)  "Last Name Frequency"
    from employees e
- order by 2, 3, 1
+order by 1 desc, 2, 3, 1
